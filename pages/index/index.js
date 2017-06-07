299eaa3d1c3fc84b5133f79b517589bc77c3fbb8
yindong <yindong0720@vip.qq.com>
2017-06-07 19:47:20 +0800

    完善个人资料

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,91 @@ var app = getApp()
 Page({
   data: {
     motto: '我们都是好朋友，呱呱呱！喜欢快乐的生活最爱说笑话。',
-    userInfo: {}
+    message: 'Hello Friend',
+    userInfo: {},
+
+    array: ['看书', '听音乐', '看电影', '踢足球', '玩游戏', '旅游'],
+    objectArray: [
+      {
+        id: 0,
+        name: '看书'
+      },
+      {
+        id: 1,
+        name: '听音乐'
+      },
+      {
+        id: 2,
+        name: '看电影'
+      },
+      {
+        id: 3,
+        name: '踢足球'
+      },
+      {
+        id: 4,
+        name: '玩游戏'
+      },
+      {
+        id: 5,
+        name: '旅游'
+      }
+    ],
+    index: 0,
+    feelingArray: ['单身', '已婚', '恋爱'],
+    feelingObjectArray: [
+      {
+        id: 0,
+        name: '单身'
+      },
+      {
+        id: 1,
+        name: '已婚'
+      },
+      {
+        id: 2,
+        name: '恋爱'
+      }
+    ],
+    feelingIndex:0,
+    faithArray: ['基督教', '佛教', '伊斯兰教'],
+    faithObjectArray: [
+      {
+        id: 0,
+        name: '基督教'
+      },
+      {
+        id: 1,
+        name: '佛教'
+      },
+      {
+        id: 2,
+        name: '伊斯兰教'
+      }
+    ],
+    faithIndex:0
+  },
+  bindfeelingViewTap: function () {
+    console.log(document);
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindFeelingChange: function (e) {
+    this.setData({
+      feelingIndex: e.detail.value
+    })
+  },
+  bindFaithChange: function (e) {
+    this.setData({
+      faithIndex: e.detail.value
+    })
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -16,10 +97,10 @@ Page({
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
